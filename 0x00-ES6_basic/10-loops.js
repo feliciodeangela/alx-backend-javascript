13e2ee93d8f6bb511ec10da992373d1a1c475b41
feliciodeangela <feliciodeangela@gmail.com>
2024-03-18 00:35:38 +0200

    repo recovery

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const el of array) {
+    newArray.push(`${appendString}${el}`);
+  }
+
+  return newArray;
+}
